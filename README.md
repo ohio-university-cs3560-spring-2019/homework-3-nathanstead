d0f48e7feff3b34e6e8934e42a37b06c17b3b765
Nathan Steadman <nathansteadman@Computers-MacBook-Pro.local>
2019-02-11 17:53:58 -0500

    Adding the separate pdf for standard deviation description

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 by Nathan Steadman
    Ohio University
+   CS 3560
    2019 February 11
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+double deviation(int* a, int n);
+
+int main()
+{
+	  int a[4] = {1, 2, 3,4};
+		std::cout << deviation(a, 4) << std::endl;
+}
