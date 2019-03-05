b8bbaf3fcb408de8746164f3923d679139e6df2b
Nathan Steadman <nathansteadman@Computers-MacBook-Pro.local>
2019-03-04 23:56:49 -0500

    hw7 files

---
+++ hw7/hw7.cc
@@ -0,0 +1,41 @@
+// Nathan Steadman
+// Ohio University
+// CS 3560 Hw7
+// 2019 March 4
+
+#include <iostream>
+#include <cstdarg>
+#include <cmath>
+#include "hw7.h"
+using namespace std;
+
+double stddev(int n, ...){
+    va_list args;
+    va_start(args, n);  // Receiving how many arguments there will be
+    int nums[n];
+    for (int i = 0; i < n; i++) {
+    	int in = va_arg(args, int);
+      nums[i]  = in;  // Putting the numbers into the array
+    }
+
+    double mean = sum / n;
+  	double stddev = 0;
+
+  	for (size_t i = 0; i < n; ++i) {
+  		stddev = stddev + (nums[i] - mean) * (nums[i] - mean);
+  	}
+
+  	stddev /= n;
+
+  	if (stddev == 0)
+  		std::cout << "Sigma is zero." << std::endl;
+
+  	return sqrt(stddev);
+
+    va_end(args);
+}
+
+int main()
+{
+    cout << "The standard deviation is: " << stddev(4, 1, 2, 3, 4) << endl;
+}
---
+++ hw7/hw7.h
@@ -0,0 +1,11 @@
+// Nathan Steadman
+// Ohio University
+// CS 3560 Hw7
+// 2019 March 4
+
+#ifndef HW7_H
+#define HW7_H
+
+double stddev(int n, ...);
+
+#endif
